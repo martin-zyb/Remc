7c8e8f58e6144eb05c346da215734d09c3a85cf4
martin-zyb <aszyb07@126.com>
2022-07-28 15:16:57 +0800

    Use premake to support multiple platforms

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Remc"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}/%{cfg.architecture}"
+
+project "Remc"
+		location "Remc"
+		kind "SharedLib"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"%{prj.name}/vendor/spdlog/include"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "10.0.19041.0"
+
+			defines
+			{
+				"REMC_PLATFORM_WINDOWS",
+				"REMC_BUILD_DLL"
+			}
+
+			postbuildcommands
+			{
+				("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			}
+
+		filter "configurations:Debug"
+			defines "REMC_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "REMC_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "REMC_DIST"
+			optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Remc/vendor/spdlog/include",
+		"Remc/src"
+	}
+
+	links
+	{
+		"Remc"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"REMC_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "REMC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "REMC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "REMC_DIST"
+		optimize "On"
+
