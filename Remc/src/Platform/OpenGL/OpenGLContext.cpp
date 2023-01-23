091023e9d8f421994af8ae6722a697cae9d1de11
martin-zyb <aszyb07@126.com>
2023-01-23 12:16:36 +0800

    The first triangle

--- Remc/src/Platform/OpenGL/OpenGLContext.cpp
+++ Remc/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,10 +18,21 @@ namespace Remc
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		REMC_CORE_ASSERT(status, "Failed to initialize Glad!");
+		REMC_CORE_INFO("OpenGL Info:");
+		REMC_CORE_INFO("   Vendor: {0}", GLubyteToString(glGetString(GL_VENDOR)));
+		REMC_CORE_INFO("   Renderer: {0}", GLubyteToString(glGetString(GL_RENDERER)));
+		REMC_CORE_INFO("   Version: {0}", GLubyteToString(glGetString(GL_VERSION)));
 	}
 
 	void OpenGLContext::SwapBuffers()
 	{
 		glfwSwapBuffers(m_WindowHandle);
+	}
+
+	std::string OpenGLContext::GLubyteToString(const unsigned char* glbyte)
+	{
+		std::string str;
+		str.append(reinterpret_cast<const char*>(glbyte));
+		return str;
 	}
 }
--- Remc/src/Platform/OpenGL/OpenGLContext.h
+++ Remc/src/Platform/OpenGL/OpenGLContext.h
@@ -10,6 +10,8 @@ namespace Remc
 	{
 	private:
 		GLFWwindow* m_WindowHandle;
+
+		std::string GLubyteToString(const unsigned char* glbyte);
 	public:
 		OpenGLContext(GLFWwindow* windowHandle);
 
