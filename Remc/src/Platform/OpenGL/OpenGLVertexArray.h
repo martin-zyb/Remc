e86fc5aa49ec824190c98f82cfe6230564a35965
martin-zyb <aszyb07@126.com>
2023-01-25 23:52:49 +0800

    Fix warning casting size_t to uint32_t in OpenGLVertexArray

--- Remc/src/Platform/OpenGL/OpenGLVertexArray.h
+++ Remc/src/Platform/OpenGL/OpenGLVertexArray.h
@@ -20,7 +20,7 @@ namespace Remc {
 		virtual const Ref<IndexBuffer>& GetIndexBuffer() const { return m_IndexBuffer; }
 	private:
 		uint32_t m_RendererID;
-		uint32_t m_VertexBufferIndexOffset = 0;
+		uint32_t m_VertexBufferIndex = 0;
 		std::vector<Ref<VertexBuffer>> m_VertexBuffers;
 		Ref<IndexBuffer> m_IndexBuffer;
 	};
