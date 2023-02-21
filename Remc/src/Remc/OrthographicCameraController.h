df63e7264aa5f2200c5af6e798c7460e074189aa
martin-zyb <aszyb07@126.com>
2023-02-21 23:01:55 +0800

    Added proper window resize handling.

--- Remc/src/Remc/OrthographicCameraController.h
+++ Remc/src/Remc/OrthographicCameraController.h
@@ -19,6 +19,9 @@ namespace Remc
 
 		OrthographicCamera& GetCamera() { return m_Camera; }
 		const OrthographicCamera& GetCamera() const { return m_Camera; }
+
+		float GetZoomLevel() const { return m_ZoomLevel; }
+		void SetZoomLevel(float level) { m_ZoomLevel = level; }
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
