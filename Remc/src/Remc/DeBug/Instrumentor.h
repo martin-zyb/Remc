3f727db1622dba79cfcf24c36d2a276dd60f2afc
martin-zyb <aszyb07@126.com>
2023-08-03 23:18:16 +0800

    Auto deducing an availabe __FUNCSIG__ definition

--- Remc/src/Remc/DeBug/Instrumentor.h
+++ Remc/src/Remc/DeBug/Instrumentor.h
@@ -125,11 +125,33 @@ namespace Remc {
 
 #define REMC_PROFILE 1
 
-#if REMC_PROFILE
+	#if REMC_PROFILE
+	
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define REMC_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define REMC_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define REMC_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define REMC_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define REMC_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define REMC_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define REMC_FUNC_SIG __func__
+	#else
+		#define REMC_FUNC_SIG "REMC_FUNC_SIG unknown!"
+	#endif
+
 	#define REMC_PROFILE_BEGIN_SESSION(name, filepath) ::Remc::Instrumentor::Get().BeginSession(name, filepath)
 	#define REMC_PROFILE_END_SESSION() ::Remc::Instrumentor::Get().EndSession()
 	#define REMC_PROFILE_SCOPE(name) ::Remc::InstrumentationTimer timer##__LINE__(name);
-	#define REMC_PROFILE_FUNCTION() REMC_PROFILE_SCOPE(__FUNCSIG__)
+	#define REMC_PROFILE_FUNCTION() REMC_PROFILE_SCOPE(REMC_FUNC_SIG)
 #else
 	#define REMC_PROFILE_BEGIN_SESSION(name, filepath)
 	#define REMC_PROFILE_END_SESSION()
