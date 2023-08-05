6edc4b754a03fc75a80f8e4e535b34d6211c2201
martin-zyb <aszyb07@126.com>
2023-08-05 15:54:34 +0800

    Renderer2D can now render rotated quads in batches and use transforms

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -36,14 +36,18 @@ void Sandbox2D::OnUpdate(Remc::Timestep ts)
 	}
 
 	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		REMC_PROFILE_SCOPE("Renderer Draw");
+
 		Remc::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		//Remc::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), {0.8f, 0.2f, 0.3f, 1.0f});
+		Remc::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
 		Remc::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Remc::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 		//Remc::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f, glm::vec4( 0.9f, 1.0f, 0.75f, 1.0f ) );
-		Remc::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		Remc::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);
+		Remc::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Remc::Renderer2D::DrawRotatedQuad({ 0.75f, 0.75f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
 		Remc::Renderer2D::EndScene();
 	}
 }
