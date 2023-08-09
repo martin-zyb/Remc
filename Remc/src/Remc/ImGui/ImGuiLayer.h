23a7bb35a85aa25354c258b1bb602ee332088dd7
yhhl06 <yhhl20060713@163.com>
2023-08-09 14:36:04 +0800

    Imgui handling mouse and keyboards events

--- Remc/src/Remc/ImGui/ImGuiLayer.h
+++ Remc/src/Remc/ImGui/ImGuiLayer.h
@@ -16,6 +16,7 @@ namespace Remc {
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
+		virtual void OnEvent(Event& e) override;
 		// virtual void OnImGuiRender() override;
 
 		void Begin();
