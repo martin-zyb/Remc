ee24659de3b2e2523cbab17ff19e46f239bafb4a
martin-zyb <aszyb07@126.com>
2023-08-10 18:58:42 +0800

    Optimization in Instrumentor.h

--- Remc/src/Remc/DeBug/Instrumentor.h
+++ Remc/src/Remc/DeBug/Instrumentor.h
@@ -70,14 +70,11 @@ namespace Remc {
 		{
 			std::stringstream json;
 
-			std::string name = result.Name;
-			std::replace(name.begin(), name.end(), '"', '\'');
-
 			json << std::setprecision(3) << std::fixed;
 			json << ",{";
 			json << "\"cat\":\"function\",";
 			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-			json << "\"name\":\"" << name << "\",";
+			json << "\"name\":\"" << result.Name << "\",";
 			json << "\"ph\":\"X\",";
 			json << "\"pid\":0,";
 			json << "\"tid\":" << result.ThreadID << ",";
@@ -153,6 +150,36 @@ namespace Remc {
 		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 		bool m_Stopped;
 	};
+
+	namespace InstrumentorUtils {
+
+		template <size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+		};
+
+		template <size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result = {};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+			return result;
+		}
+	}
+
 }
 
 #define REMC_PROFILE 0
@@ -164,7 +191,7 @@ namespace Remc {
 		#define REMC_FUNC_SIG __PRETTY_FUNCTION__
 	#elif defined(__DMC__) && (__DMC__ >= 0x810)
 		#define REMC_FUNC_SIG __PRETTY_FUNCTION__
-	#elif defined(__FUNCSIG__)
+	#elif (defined(__FUNCSIG__) || (_MSC_VER))
 		#define REMC_FUNC_SIG __FUNCSIG__
 	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
 		#define REMC_FUNC_SIG __FUNCTION__
@@ -180,7 +207,8 @@ namespace Remc {
 
 	#define REMC_PROFILE_BEGIN_SESSION(name, filepath) ::Remc::Instrumentor::Get().BeginSession(name, filepath)
 	#define REMC_PROFILE_END_SESSION() ::Remc::Instrumentor::Get().EndSession()
-	#define REMC_PROFILE_SCOPE(name) ::Remc::InstrumentationTimer timer##__LINE__(name);
+	#define HZ_PROFILE_SCOPE(name) constexpr auto fixedName = ::Remc::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+						::Remc::InstrumentationTimer timer##__LINE__(fixedName.Data)
 	#define REMC_PROFILE_FUNCTION() REMC_PROFILE_SCOPE(REMC_FUNC_SIG)
 #else
 	#define REMC_PROFILE_BEGIN_SESSION(name, filepath)
