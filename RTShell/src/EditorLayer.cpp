8405ff7b14628203c10ab1f0ddc7ebcbfa51c5af
martin-zyb <aszyb07@126.com>
2023-09-16 10:50:46 +0800

    Changed ScriptableEntity functions to be virtual.

--- RTShell/src/EditorLayer.cpp
+++ RTShell/src/EditorLayer.cpp
@@ -42,6 +42,8 @@ namespace Remc {
 		public:
 			void OnCreate()
 			{
+				auto& transform = GetComponent<TransformComponent>().Transform;
+				transform[3][0] = rand() % 10 - 5.0f;
 			}
 
 			void OnDestroy()
@@ -65,6 +67,7 @@ namespace Remc {
 		};
 
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+		m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 	}
 
 	void EditorLayer::OnDetach()
