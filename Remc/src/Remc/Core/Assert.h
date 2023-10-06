7430bd73a96f171ff035365bae9423b31d79242e
martin-zyb <aszyb07@126.com>
2023-10-06 23:10:32 +0800

    Replace filename extraction with equivalent standard library function.

--- Remc/src/Remc/Core/Assert.h
+++ Remc/src/Remc/Core/Assert.h
@@ -3,29 +3,15 @@
 #include "Remc/Core/Base.h"
 #include "Remc/Core/Log.h"
 
-#ifdef REMC_ENABLE_ASSERTS
+#include <filesystem>
 
-namespace Remc::Assert
-{
-	// Returns the simple file name rather than full path as suggested by LovelySanta
-	constexpr const char* CurrentFileName(const char* path) {
-		const char* file = path;
-		while (*path)
-		{
-			if (*path == '/' || *path == '\\')
-				file = ++path;
-			else
-				path++;
-		}
-		return file;
-	}
-}
+#ifdef REMC_ENABLE_ASSERTS
 
 // Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 // provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 #define REMC_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { REMC##type##ERROR(msg, __VA_ARGS__); REMC_DEBUGBREAK(); } }
 #define REMC_INTERNAL_ASSERT_WITH_MSG(type, check, ...) REMC_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-#define REMC_INTERNAL_ASSERT_NO_MSG(type, check) REMC_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", REMC_STRINGIFY_MACRO(check), ::Remc::Assert::CurrentFileName(__FILE__), __LINE__)
+#define REMC_INTERNAL_ASSERT_NO_MSG(type, check) REMC_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", REMC_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 #define REMC_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 #define REMC_INTERNAL_ASSERT_GET_MACRO(...) REMC_EXPAND_MACRO( REMC_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, REMC_INTERNAL_ASSERT_WITH_MSG, REMC_INTERNAL_ASSERT_NO_MSG) )
