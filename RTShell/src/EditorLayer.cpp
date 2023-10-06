ddada740cc96bf6d16357b2db09dc25ac62da768
martin-zyb <aszyb07@126.com>
2023-10-06 22:53:05 +0800

    Added Sprite Renderer Component UI

--- RTShell/src/EditorLayer.cpp
+++ RTShell/src/EditorLayer.cpp
@@ -33,10 +33,10 @@ namespace Remc {
 
 		m_SquareEntity = square;
 
-		m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
+		m_CameraEntity = m_ActiveScene->CreateEntity("Camera A");
 		m_CameraEntity.AddComponent<CameraComponent>();
 
-		m_SecondCamera = m_ActiveScene->CreateEntity("Clip-Space Entity");
+		m_SecondCamera = m_ActiveScene->CreateEntity("Camera B");
 		auto& cc = m_SecondCamera.AddComponent<CameraComponent>();
 		cc.Primary = false;
 
@@ -212,7 +212,7 @@ namespace Remc {
 
 			m_SceneHierarchyPanel.OnImGuiRender();
 
-			ImGui::Begin("Settings");
+			ImGui::Begin("Stats");
 
 			ImGui::Checkbox("Color Grid", &Color_grid);
 
@@ -223,38 +223,6 @@ namespace Remc {
 			ImGui::Text("	Vertices:	  %d", stats.GetTotalVertexCount());
 			ImGui::Text("	Indices:	   %d", stats.GetTotalIndexCount());
 
-			ImGui::Text("\n");
-
-			if (m_SquareEntity)
-			{
-				ImGui::Separator();
-				auto& tag = m_SquareEntity.GetComponent<TagComponent>().Tag;
-				ImGui::Text("%s", tag.c_str());
-
-				auto& squareColor = m_SquareEntity.GetComponent<SpriteRendererComponent>().Color;
-				ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
-				ImGui::Separator();
-			}
-			ImGui::Text("\n");
-
-			ImGui::DragFloat3("Camera Transform",
-				glm::value_ptr(m_CameraEntity.GetComponent<TransformComponent>().Transform[3]));
-
-			if (ImGui::Checkbox("Camera A", &m_PrimaryCamera))
-			{
-				m_CameraEntity.GetComponent<CameraComponent>().Primary = m_PrimaryCamera;
-				m_SecondCamera.GetComponent<CameraComponent>().Primary = !m_PrimaryCamera;
-			}
-			ImGui::Text("\n");
-
-			{
-				auto& camera = m_SecondCamera.GetComponent<CameraComponent>().Camera;
-				float orthoSize = camera.GetOrthographicSize();
-				if (ImGui::DragFloat("Second Camera Ortho Size", &orthoSize))
-				{
-					camera.SetOrthographicSize(orthoSize);
-				}
-			}
 			ImGui::Text("\n");
 
 			ImGui::End();
