263af36494c26e44b3c5ddd96027bc1dda53f22c
martin-zyb <aszyb07@126.com>
2023-10-18 21:28:35 +0800

    Fix that Event::Handled could be set back to false.

--- Remc/src/Remc/Events/Event.h
+++ Remc/src/Remc/Events/Event.h
@@ -69,7 +69,7 @@ namespace Remc {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(static_cast<T&>(m_Event));
+				m_Event.Handled |= func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
