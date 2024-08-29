5e6a25d2c851705adfc4755ba214e7d3ee7584a4
martin-zyb <aszyb07@126.com>
2024-08-29 18:45:08 +0800

    Fixed Math function declaration

--- Remc/src/Remc/Math/Math.h
+++ Remc/src/Remc/Math/Math.h
@@ -5,6 +5,6 @@
 namespace Remc::Math
 {
 
-	bool DecomposeTransform(const glm::mat4& transform, glm::vec3& outTranslation, glm::vec3& outRotation, glm::vec3& outScale);
+	bool DecomposeTransform(const glm::mat4& transform, glm::vec3& translation, glm::vec3& rotation, glm::vec3& scale);
 
 }
