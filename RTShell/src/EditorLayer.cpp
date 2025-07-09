311e49c3e1847515a268e857c33360c7336f2ba6
martin-zyb <aszyb07@126.com>
2025-07-09 14:08:33 +0800

    Refactored framebuffers to allow for MRTs

--- RTShell/src/EditorLayer.cpp
+++ RTShell/src/EditorLayer.cpp
@@ -25,6 +25,7 @@ namespace Remc {
 		m_CheckerboardTexture = Texture2D::Create("assets/textures/Checkerboard.png");
 
 		FramebufferSpecification fbSpec;
+		fbSpec.Attachments = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::Depth };
 		fbSpec.Width = 1280;
 		fbSpec.Height = 720;
 		m_Framebuffer = Framebuffer::Create(fbSpec);
